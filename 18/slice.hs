e796c8fe869b04e5b82e331abfb57d1cd7c50a28
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-10-28 11:11:04 -0400

    Update slice.hs

--- 18/slice.hs
+++ 18/slice.hs
@@ -3,4 +3,4 @@ slice from to xs = foldr f [] xs' where
     f (i, item) items
         | from <= i && i <= to = item : items
         | otherwise            = items
-    xs' = zip [1,2..] xs
+    xs' = zip [1..] xs
