2a1d39876434643c369ac7f17359f98fa342f4c7
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-11-06 10:30:00 -0500

    added another solution to problem about generating a list of consecutive numbers

--- 22/range.hs
+++ 22/range.hs
@@ -1,2 +1,9 @@
 range :: Enum a => a -> a -> [a]
 range a b = [a..b]
+
+
+range' :: (Ord a, Enum a) => a -> a -> [a]
+range' a b
+    | a <= b     = a : range' a' b
+    | otherwise  = []
+    where a' = succ a
